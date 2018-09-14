fa9d206ed077d0f8eb8c76ca020879eb3aa7f129
zyuwei <1611515@mail.nankai.edu.cn>
2018-09-14 14:30:49 -0400

    Add files via upload

--- ednn-c-gpus.py
+++ ednn-c-gpus.py
@@ -34,22 +34,24 @@ def average_tower_grads( tower_grads):
 
 
 def build_model(L, f, c, train_data, train_labels, valid_data,
-                valid_labels, save_dir):
-    # data comes in a [ batch * L * L ] tensor, and labels a [ batch * 1] tensor
-    with tf.device("/cpu:0"):
-      x = tf.placeholder(tf.float32, (None, L, L), name='input_image')
-      y = tf.placeholder(tf.float32, (None, 1))
-      helper = EDNN_helper(L=L, f=f, c=c)
-      optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-      towerGrads = []
-      towerloss = []
-      tiles = tf.map_fn(helper.ednn_split, x, back_prop=False)
-      tiles = tf.transpose(tiles, perm=[1, 0, 2, 3, 4])
-    with tf.variable_scope('train') as scope:
+                valid_labels, save_dir, summary_dir):
+    a = tf.Graph()
+    with a.as_default():
+      # data comes in a [ batch * L * L ] tensor, and labels a [ batch * 1] tensor
+      with tf.device("/cpu:0"):
+        x = tf.placeholder(tf.float32, (2, None, L, L), name='input_image')
+        y = tf.placeholder(tf.float32, (2, None, 1))
+        helper = EDNN_helper(L=L, f=f, c=c)
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        towerGrads = []
+        towerloss = []
+      with tf.variable_scope('train') as scope:
         for i in range(2):
             with tf.device("/gpu:%d" % i):
                 with tf.name_scope('tower_%d' % i) as scope:
                     # Then the EDNN-specific code:
+                    tiles = tf.map_fn(helper.ednn_split, x[i], back_prop=False)
+                    tiles = tf.transpose(tiles, perm=[1, 0, 2, 3, 4])
                     output = tf.map_fn(NN_coarse, tiles, back_prop=True)
                     output = tf.transpose(output, perm=[1, 0, 2])
                     predicted = tf.reduce_sum(output, axis=1)
@@ -57,58 +59,62 @@ def build_model(L, f, c, train_data, train_labels, valid_data,
                     vars = tf.trainable_variables()
                     lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in vars
                                        if 'bias' not in v.name]) * 0.001
-                    loss_ = tf.reduce_mean(tf.square(y - predicted)) + lossL2
+                    loss_ = tf.reduce_mean(tf.square(y[i] - predicted)) + lossL2
                     towerloss.append(loss_)
                     towerGrads.append(optimizer.compute_gradients(loss_))
                     tf.get_variable_scope().reuse_variables()
-    avg_Grads = average_tower_grads(towerGrads)
-    train_step = optimizer.apply_gradients(avg_Grads)
-    loss = tf.reduce_sum(towerloss) / 2
-    init = tf.global_variables_initializer()
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.InteractiveSession(config=config)
-    sess.run(init)
-    saver = tf.train.Saver()
+      avg_Grads = average_tower_grads(towerGrads)
+      train_step = optimizer.apply_gradients(avg_Grads)
+      loss = tf.reduce_sum(towerloss) / 2
+      tf.summary.scalar('loss', loss)
+      init = tf.global_variables_initializer()
+      config = tf.ConfigProto()
+      config.gpu_options.allow_growth = True
+      sess = tf.InteractiveSession(config=config)
+      merged = tf.summary.merge_all()
+      train_writer = tf.summary.FileWriter(summary_dir + '/train', sess.graph)
+      valid_writer = tf.summary.FileWriter(summary_dir + '/valid')
+      sess.run(init)
+      saver = tf.train.Saver()
+      a.finalize()
     model_path = os.path.join(save_dir, 'model')
     BATCH_SIZE = 5000
     EPOCHS = 5000
     for epoch in range(EPOCHS):
-        for batch in range(int(train_data.shape[0] / BATCH_SIZE)):
-            _, loss_tra = sess.run([train_step, loss],
+        for batch in range(int(train_data.shape[0] / BATCH_SIZE / 2)):
+            _, loss_tra, summary1 = sess.run([train_step, loss, merged],
                        feed_dict={
-                            x: train_data[batch*BATCH_SIZE:(batch+1)*BATCH_SIZE],
-                            y: train_labels[batch*BATCH_SIZE:(batch+1)*BATCH_SIZE]
+                            x: [train_data[2*batch*BATCH_SIZE:(2*batch+1)*BATCH_SIZE], train_data[(2*batch+1)*BATCH_SIZE:2*(batch+1)*BATCH_SIZE]], 
+                            y: [train_labels[2*batch*BATCH_SIZE:(2*batch+1)*BATCH_SIZE], train_labels[(2*batch+1)*BATCH_SIZE:2*(batch+1)*BATCH_SIZE]]
                         }
                       )
 
             if batch % 1000 == 0:
-                loss_val = sess.run(loss,
+                loss_val, summary2 = sess.run([loss, merged], 
                                     feed_dict={
-                                        x: valid_data,
-                                        y:valid_labels
+                                        x: [valid_data[:int(len(valid_data)/2)], valid_data[int(len(valid_data)/2):]], 
+                                        y: [valid_labels[:int(len(valid_data)/2)], valid_labels[int(len(valid_data)/2):]]
                                     }
                                     )
-                print("epoch: " + str(epoch) + ' | training loss: ' + str(loss_tra)
-                      + ' | validation loss: ' + str(loss_val) + " (model saved)")
+                train_writer.add_summary(summary1, epoch)
+                valid_writer.add_summary(summary2, epoch)
+                print "epoch: " + str(epoch) + ' | training loss: ' + str(loss_tra) \
+                      + ' | validation loss: ' + str(loss_val) + " (model saved)"
                 saver.save(sess, model_path)
-    sess.close()
-
 
 if __name__ == '__main__':
     L = 32
     f_c = 8
     c_c = 8
 
-    f1 = h5py.File('train-data2/data.hdf5', 'r')
-    f2 = h5py.File('valid-data2/data.hdf5', 'r')
-    f3 = h5py.File('test-data2/data.hdf5', 'r')
+    f1 = h5py.File('train-data3/data.hdf5', 'r')
+    f2 = h5py.File('valid-data3/data.hdf5', 'r')
+    f3 = h5py.File('test-data3/data.hdf5', 'r')
 
     train_data = f1['data']
     train_labels = np.reshape(f1['elecenergy'], [-1, 1])
     valid_data = f2['data']
     valid_labels = np.reshape(f2['elecenergy'], [-1, 1])
 
-    with tf.Graph().as_default():
-        build_model(L, f_c, c_c, train_data, train_labels, valid_data,
-                    valid_labels, 'coarse_model')
+    build_model(L, f_c, c_c, train_data, train_labels, valid_data,
+                    valid_labels, 'coarse_model', 'summary')
